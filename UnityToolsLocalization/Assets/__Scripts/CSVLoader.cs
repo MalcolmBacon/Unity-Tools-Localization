dbeea75329ad40d799b9d0a3a320fa59e0c0e6f4
Malcolm Bacon <mbacon23@uwo.ca>
2020-11-02 23:33:37 -0500

    Created localized system that reads key value pair from csv file

---
+++ UnityToolsLocalization/Assets/__Scripts/CSVLoader.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class CSVLoader
+{
+    private TextAsset csvFile;
+
+    // Start is called before the first frame update
+    public void LoadCSV()
+    {
+        csvFile = Resources.Load<TextAsset>("localization");
+    }
+    public Dictionary<string, string> GetDictionaryValuesForLanguage(string attributeID)
+    {
+        char lineSeperator = '\n';
+        string[] fieldSeperator = new string[] { "\",\"" };
+
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        string[] lines = csvFile.text.Split(lineSeperator);
+
+        Regex regex = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"); // split string on commas followed by an even number of double quotes
+
+        int attributeIndex = -1;
+
+        string[] header = regex.Split(lines[0]);
+
+        for (int j = 1; j < header.Length; j++)
+        {
+            header[j] = header[j].Trim();
+            header[j] = header[j].Trim('"');
+
+            if (header[j].Equals(attributeID, System.StringComparison.OrdinalIgnoreCase))
+            {
+                attributeIndex = j;
+                break;
+            }
+        }
+
+        if (attributeIndex == -1)
+        {
+            throw new System.Exception($"{nameof(attributeID)} was not found");
+        }
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string line = lines[i];
+
+            // Skip any potentially empty lines
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
+            //Parse line based on regex
+            string[] fields = regex.Split(line);
+
+            for (int j = 0; j < fields.Length; j++)
+            {
+                fields[j] = fields[j].Trim();
+                fields[j] = fields[j].Trim('"');
+            }
+
+            // Update dictionary or skip
+            if (fields.Length > attributeIndex)
+            {
+                string key = fields[0];
+
+                if (dict.ContainsKey(key))
+                {
+                    continue;
+                }
+
+                string value = fields[attributeIndex];
+
+                dict.Add(key, value);
+            }
+        }
+        return dict;
+    }
+}
---
+++ UnityToolsLocalization/Assets/__Scripts/LocalizationSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationSystem 
+{
+    public enum Language
+    {
+        English, 
+        French
+    }
+    public static Language language = Language.English;
+
+    private static Dictionary<string, string> localizedEN;
+    private static Dictionary<string, string> localizedFR;
+    public static bool isInit;
+    public static void Init()
+    {
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.LoadCSV();
+
+        localizedEN = csvLoader.GetDictionaryValuesForLanguage("EN");
+        localizedFR = csvLoader.GetDictionaryValuesForLanguage("FR");
+
+        isInit = true;
+    }
+    public static string GetLocalizedValue(string key)
+    {
+        if (!isInit)
+        {
+            Init();
+        }
+
+        string value = key;
+
+        switch (language)
+        {
+            case Language.English:
+                localizedEN.TryGetValue(key, out value);
+                break;
+            case Language.French:
+                localizedFR.TryGetValue(key, out value);
+                break;
+        }
+
+        return value;
+    }
+}
